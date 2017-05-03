9ad2916cb85920657e2b523afbf7d0048c4c2791
Jiaowenhui <523947655@qq.com>
2017-05-04 00:13:06 +0800

    司机端ui新增

---
+++ appdriver/src/components/new/user/orderinfo.js
@@ -0,0 +1,68 @@
+﻿/*
+    个人中心-订单详情
+*/
+import React, { Component } from 'react';
+import WeUI from 'react-weui';
+import 'weui';
+import 'react-weui/lib/react-weui.min.css';
+import '../../../../public/newcss/userorderinfo.css';
+import NavBar from '../tools/nav.js';
+import StarRatingComponent from 'react-star-rating-component';
+const { 
+    
+    LoadMore
+    } = WeUI;
+
+class Page extends Component {
+
+    render() {
+        return (
+            <div className="userorderinfoPage AppPage">
+                <NavBar back={true} title="订单详情" />
+                <div className="pageContent">
+                    <div className="orderinfohead">
+                        <img src="newimg/17.png" className="avatar"/>
+                        <div className="address">
+                            <div>江宁区东山路214号成安国际大厦</div>
+                            <div>建邺区东山路214号成安国际大厦</div>
+                        </div>
+                        <img src="newimg/19.png" className="phone" />
+                    </div>
+
+                    <div className="content">
+                        <LoadMore showLine>已支付</LoadMore>
+                        <span className="price color_warning">121.0元</span>
+                    </div>
+
+
+                    <div className="pricelist PanelBox">
+                        <div className="tit">车费情况</div>
+                        <div className="list">
+                            <p><span>起步价</span><span>0.0元</span></p>
+                            <p><span>里程费(23.5公里)</span><span>71.0元</span></p>
+                            <p><span>时长费(76分钟)</span><span>30.4元</span></p>
+                            <p><span>高速费</span><span>120.0元</span></p>
+                            <p><span className="color_warning">4.5折券抵扣(最高5元)</span><span className="color_warning">-5.0元</span></p>
+                        </div>
+                    </div>
+
+                    <div className="tt">评价乘客</div>
+
+                    <div className="evaluate PanelBox">
+                        <StarRatingComponent 
+                            name="star" 
+                            editing={false}
+                            starCount={5}
+                            value={4.5}
+                            emptyStarColor="#EEEEEE"
+                        />
+                        <span className="text">默认好评</span>
+                    </div>
+
+                    <div className="getMoney"><span>收现金</span></div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Page;
---
+++ appdriver/src/components/new/user/orderlist.js
@@ -0,0 +1,43 @@
+﻿/*
+    个人中心-订单详情
+*/
+import React, { Component } from 'react';
+import WeUI from 'react-weui';
+import 'weui';
+import 'react-weui/lib/react-weui.min.css';
+import '../../../../public/newcss/userorderlist.css';
+import NavBar from '../tools/nav.js';
+const { 
+    
+    Cells,
+    Cell,
+    CellBody,
+    CellFooter
+    } = WeUI;
+
+class Page extends Component {
+
+    render() {
+        return (
+            <div className="userorderlistPage AppPage">
+                <NavBar back={true} title="我的订单" />
+                <div className="list">
+                    <Cells>
+                        <Cell href="javascript:;" access>
+                            <CellBody>
+                                <div className="tt">
+                                    <span className="i">预约</span>
+                                    <span>2017-01-22 10:00</span>
+                                </div>
+                                <div className="li a">我的行程</div>
+                                <div className="li b">我的行程</div>
+                            </CellBody>
+                            <CellFooter />
+                        </Cell>
+                    </Cells>
+                </div>
+            </div>
+        )
+    }
+}
+export default Page;
