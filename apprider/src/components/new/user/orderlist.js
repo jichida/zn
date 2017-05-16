1c7a571564d89cb39f8560dce77f6ac954dbb5f6
Jiaowenhui <523947655@qq.com>
2017-05-16 20:29:15 +0800

    no message

--- apprider/src/components/new/user/orderlist.js
+++ apprider/src/components/new/user/orderlist.js
@@ -7,6 +7,12 @@ import 'weui';
 import 'react-weui/lib/react-weui.min.css';
 import '../../../../public/newcss/userorderlist.css';
 import NavBar from '../tools/nav.js';
+import _ from "lodash";
+import { connect } from 'react-redux';
+import moment from "moment";
+import {
+    getmytriporders_request
+    } from '../../../actions';
 const {
     Cells,
     Cell,
@@ -16,32 +22,63 @@ const {
 
 class Page extends Component {
 
+    componentWillMount () {
+        this.props.dispatch(getmytriporders_request({
+            query: {},
+            options:{
+                sort:{created_at:-1},
+                offset: 0,
+                limit: 1000,
+            }
+        }));
+    }
+
     render() {
+        const {mytriporderlist, triporders, history} = this.props;
         return (
             <div className="userorderlistPage AppPage">
                 <NavBar back={true} title="我的订单" />
                 <div className="list">
                     <Cells>
-                        <Cell access>
-                            
-                            <CellBody>
-                                <div className="tt">
-                                    <div className="ttinfo">
-                                        <span className="i">预约</span>
-                                        <span className="time">2017-01-22 10:00</span>
-                                        <span className="type">代驾</span>
-                                    </div>
-                                    <span className="status color_warning">待支付</span>
-                                </div>
-                                <div className="li a">我的行程</div>
-                                <div className="li b">我的行程</div>
-                            </CellBody>
-                            <CellFooter />
-                        </Cell>
+                        {
+                            _.map(mytriporderlist,(orderid,index)=>{
+                                let orderinfo = triporders[orderid];
+                                console.log(orderinfo);
+                                return (
+                                    <Cell
+                                        onClick={()=>{history.push(`/orderdetail/${orderinfo._id}`);}}
+                                        access>
+                                        <CellBody>
+                                            <div className="tt">
+                                                <div className="ttinfo">
+                                                    <span className="i">预约</span>
+                                                    <span className="time">{moment(orderinfo.created_at).format("YYYY-MM-DD H:mm:ss")}</span>
+                                                    <span className="type">{orderinfo.triptype}</span>
+                                                </div>
+                                                <span className="status color_warning">{orderinfo.orderstatus}</span>
+                                            </div>
+                                            {orderinfo.hasOwnProperty("srcaddress")?(
+                                                <div className="li a">{orderinfo.srcaddress.addressname}</div>
+                                            ):""}
+                                            {orderinfo.hasOwnProperty("dstaddress")?(
+                                                <div className="li b">{orderinfo.dstaddress.addressname}</div>
+                                            ):""}
+                                        </CellBody>
+                                        <CellFooter />
+                                    </Cell>
+                                )
+                            })
+                        }
+                        
                     </Cells>
                 </div>
             </div>
         )
     }
 }
-export default Page;
+const data =({myorders})=>{
+    return {...myorders};
+};
+export default connect(data)(Page);
+
+
