456fc68b72803897d4f92664ce72cecc7511246a
wangxiaoqing <xiaoqingwang@126.com>
2017-06-12 21:06:43 +0800

    push调试

--- appdriver/src/sagas/jpushflow.js
+++ appdriver/src/sagas/jpushflow.js
@@ -17,7 +17,7 @@ let async_setJPushAlias =(userid)=> {
   return new Promise(resolve => {
     setJPushAlias(userid);
     resolve();
-   });
+  });
 };
 
 let async_cancelJPushAlisa =()=> {
@@ -29,7 +29,7 @@ let async_cancelJPushAlisa =()=> {
 
 //获取地理位置信息，封装为promise
 export function* jpushflow(){//仅执行一次
-    yield takeEvery(`${login_result}`, function*(action) {
+   yield takeEvery(`${login_result}`, function*(action) {
       let {payload:{userid}} = action;
       yield call(async_setJPushAlias,userid);
       console.log(`login_result ===>${JSON.stringify(userid)}`);
@@ -42,38 +42,10 @@ export function* jpushflow(){//仅执行一次
     });
 
     yield takeEvery(`${jpushlistenInMessage}`, function*(action) {
+       let {payload:msgobj} = action;
         try{
           //yield call(alertmessage,`jpushlistenInMessage ===>${JSON.stringify(msgobj)}`);
-          let {payload:msgobj} = action;
-          let message = '接收到一条消息';
-          message = _.get(msgobj,'aps.alert',message);
-          yield put(set_weui({
-            toast:{
-            text:message,
-            show: true,
-            type:'success'
-          }}));
-          if(msgobj.hasOwnProperty('_id')){
-            yield put(push(`/mymessagedetail/${msgobj._id}`));
-          }
-          console.log(`jpushlistenInMessage ===>${JSON.stringify(msgobj)}`);
-        }
-        catch(e){
-        //  alert(`err->jpushlistenInMessage ===>${JSON.stringify(msgobj)},e:${JSON.stringify(e)}`);
-        }
-
-    });
-
-    yield takeEvery(`${jpushpostNotification}`, function*(action) {
-        // 按 2，模拟发送（点击了推送消息）
-        try{
-          //alert(`jpushpostNotification ===>${JSON.stringify(msgobj)}`);
-          //yield call(alertmessage,`jpushpostNotification ===>${JSON.stringify(msgobj)}`);
-          let {payload:msgobj} = action;
-          if(msgobj.hasOwnProperty('_id')){
-            yield put(push(`/mymessagedetail/${msgobj._id}`));
-          }
-          else{
+          if(!!msgobj){
             let message = '接收到一条消息';
             message = _.get(msgobj,'aps.alert',message);
             yield put(set_weui({
@@ -82,11 +54,46 @@ export function* jpushflow(){//仅执行一次
               show: true,
               type:'success'
             }}));
+            if(!!msgobj._id){
+              yield put(push(`/mymessagedetail/${msgobj._id}`));
+            }
+            else{
+              alert(`err->jpushlistenInMessage ===>${JSON.stringify(msgobj)}`);
+            }
           }
-          //console.log(`jpushpostNotification ===>${JSON.stringify(msgobj)}`);
+        }
+          catch(e){
+            alert(`err->jpushlistenInMessage ===>${JSON.stringify(msgobj)},e:${JSON.stringify(e)}`);
+          }
+
+    });
+
+    yield takeEvery(`${jpushpostNotification}`, function*(action) {
+      let {payload:msgobj} = action;
+        // 按 2，模拟发送（点击了推送消息）
+        try{
+          //alert(`jpushpostNotification ===>${JSON.stringify(msgobj)}`);
+          //yield call(alertmessage,`jpushpostNotification ===>${JSON.stringify(msgobj)}`);
+          if(!!msgobj){
+            if(!!msgobj._id){
+              yield put(push(`/mymessagedetail/${msgobj._id}`));
+            }
+            else{
+              alert(`err->jpushlistenInMessage ===>${JSON.stringify(msgobj)}`);
+              // let message = '接收到一条消息';
+              // message = _.get(msgobj,'aps.alert',message);
+              // yield put(set_weui({
+              //   toast:{
+              //   text:message,
+              //   show: true,
+              //   type:'success'
+              // }}));
+            }
+          }
+
         }
         catch(e){
-          //alert(`err->jpushpostNotification ===>${JSON.stringify(msgobj)},e:${JSON.stringify(e)}`);
+          alert(`err->jpushlistenInMessage ===>${JSON.stringify(msgobj)},e:${JSON.stringify(e)}`);
         }
 
     });
