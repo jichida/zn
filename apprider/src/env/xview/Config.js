bda49f8829015b5f402eef7a7d6167eb346ab000
wangxiaoqing <xiaoqingwang@126.com>
2017-06-13 16:14:34 +0800

    android端id是传入的

--- apprider/src/env/xview/Config.js
+++ apprider/src/env/xview/Config.js
@@ -2,8 +2,8 @@
 export const config=
 {
 "appName":"Xview",
-"wxappid": "wx59d5d49c9d5f47df", //微信AppID
-"wxappsecret":"d4624c36b6795d1d99dcf0547af5443d",//微信Secret
+"wxappid": "wx2b87e74e18cf8f3a", //微信AppID
+"wxappsecret":"d4624c36b6795d1d99dcf0547af5443d",//微信Secret《---注意是错误的
 "qqappid":"1104714921", //QQAppID
 "wbappkey":"3067948552",//微博AppKey
 "wbappsecret":"bf69337ef871a364b92e174855f83138", //微博Secret
