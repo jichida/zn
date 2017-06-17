522bb94cc84c77c8b34ef9ca9d956ed7576861f9
wangxiaoqing <xiaoqingwang@126.com>
2017-06-17 16:44:26 +0800

    test

--- test/src/utils/Common.js
+++ test/src/utils/Common.js
@@ -12,11 +12,10 @@
   @since 2.0
   */
  const xviewPhoneWithAppDataWithCallbackMethodWithType = (data,fun,typeString) => {
-   window.xviewCallBack=function(result){
-     result=eval("("+result+")");
+   window.xviewCallBack_xphone=function(result){
      fun(result);
    };
-   window.xview.xviewPhoneWithAppDataWithCallbackMethodWithType(JSON.stringify(data),"xviewCallBack",typeString);
+   window.xview.xviewPhoneWithAppDataWithCallbackMethodWithType(JSON.stringify(data),"xviewCallBack_xphone",typeString);
  }
  /**
    * 微信登录
@@ -155,11 +154,10 @@
    @since 2.0
    */
   export const showiPhoneAlertWithAppDataTypeWithCallbackMethod=(dataString,typeString,fun) => {
-    window.xviewCallBack=function(result){
-      result=eval("("+result+")");
+    window.xviewCallBack_showiphone=function(result){
       fun(result);
     };
-    window.xview.showiPhoneAlertWithAppDataTypeWithCallbackMethod(dataString,typeString,"xviewCallBack");
+    window.xview.showiPhoneAlertWithAppDataTypeWithCallbackMethod(dataString,typeString,"xviewCallBack_showiphone");
   }
 
   /**
@@ -177,7 +175,7 @@
    @since 2.0
    */
   export const openiPhoneApp = (url) => {
-  	if(url.indexOf("http://")!=0){
+  	if(url.indexOf("http://"!=0)){
   		url="http://"+url;
   	}
     window.xview.openiPhoneApp(url);
@@ -189,11 +187,10 @@
    @since 2.0
    */
   export const sendSmsWithPhoneNumber = (phone,fun) => {
-  	window.xviewCallBack=function(result){
-      result=eval("("+result+")");
+  	window.xviewCallBack_sendSmsWithPhoneNumber=function(result){
       fun(result);
     };
-  	window.xview.sendSmsWithPhoneNumber(phone,"xviewCallBack");
+  	window.xview.sendSmsWithPhoneNumber(phone,"xviewCallBack_sendSmsWithPhoneNumber");
   }
  /**
    * 获取经纬度
@@ -201,77 +198,25 @@
    @since 2.0
    */
   export const geographyLocationCallbackMethod = (fun) => {
-    window.callBack=function(result){
-      result=eval("("+result+")");
+    window.callBack_geographyLocation=function(result){
       fun(result);
     };
-    window.xview.geographyLocationCallbackMethod("callBack");
+    window.xview.geographyLocationCallbackMethod("callBack_geographyLocation");
   }
 
-
   /**
-    * WiFi名
-    @method currentLinkWifi
+    * 设置推送用户名
+    @method jiGuangTuiSong
     @since 2.0
     */
-   export const currentLinkWifi = (fun) => {
-     window.currentLinkWifiCallBack=function(result){
-       result=eval("("+result+")");
-       fun(result);
-     };
-     window.xview.currentLinkWifi({callback:"currentLinkWifiCallBack"});
+   export const jiGuangTuiSong = (name) => {
+     window.xview.jiGuangTuiSong(name);
    }
    /**
-     * 连接mico
-     @method prepareEasyLink
+     * 取消单推
+     @method cancelJPushAlisa
      @since 2.0
      */
-    export const prepareEasyLink = (json,fun) => {
-      window.prepareEasyLinkCallBack=function(result){
-        result=eval("("+result+")");
-        fun(result);
-      };
-      json['callback']="prepareEasyLinkCallBack";
-      window.xview.prepareEasyLink(json);
+    export const cancelJPushAlisa = () => {
+      window.xview.cancelJPushAlisa();
     }
-    /**
-      * 搜索mico
-      @method searchForModules
-      @since 2.0
-      */
-     export const searchForModules = (fun) => {
-       window.searchForModulesCallBack=function(result){
-         result=eval("("+result+")");
-         fun(result);
-       };
-       window.xview.searchForModules({callback:"searchForModulesCallBack"});
-     }
-
-     /**
-       * 获取通讯录
-       @method getPhoneBook
-       @since 2.0
-       */
-      export const getPhoneBook = (fun) => {
-        window.getPhoneBookCallback=function(result){
-          result=eval("("+result+")");
-          fun(result);
-        };
-        window.xview.getPhoneBook("getPhoneBookCallback");
-      }
-      /**
-        * 设置推送用户名
-        @method jiGuangTuiSong
-        @since 2.0
-        */
-       export const jiGuangTuiSong = (name) => {
-         window.xview.jiGuangTuiSong(name);
-       }
-       /**
-         * 取消单推
-         @method cancelJPushAlisa
-         @since 2.0
-         */
-        export const cancelJPushAlisa = () => {
-          window.xview.cancelJPushAlisa();
-        }
