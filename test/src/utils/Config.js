9ba9f0c4c092a3bb5100a47acacf5a32a9b95f0d
wangxiaoqing <xiaoqingwang@126.com>
2017-06-18 09:46:29 +0800

    for test

--- test/src/utils/Config.js
+++ test/src/utils/Config.js
@@ -2,9 +2,9 @@
 export const config=
 {
 "appName":"Xview",
-"wxappid": "wx2be56c97af373c6b", //微信AppID
-"wxappsecret":"a6de9e183ef1938d617b93bdb6aafb42",//微信Secret
-"qqappid":"101372126", //QQAppID
+"wxappid": "wx2b87e74e18cf8f3a", //微信AppID
+"wxappsecret":"d4624c36b6795d1d99dcf0547af5443d",//微信Secret《---注意是错误的
+"qqappid":"1104714921", //QQAppID
 "wbappkey":"3067948552",//微博AppKey
 "wbappsecret":"bf69337ef871a364b92e174855f83138", //微博Secret
 "wbRedirectUrl":"https://api.weibo.com/oauth2/default.html",//微博回调网址（开放平台设置）
