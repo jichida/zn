a734e9f1a71ceadf83ce19b2c20d76fac8d64df3
wangxiaoqing <xiaoqingwang@126.com>
2017-06-18 10:26:03 +0800

    注意：这是临时调试，为ios8，正式需要还原！！！！

--- apprider/src/env/geo.js
+++ apprider/src/env/geo.js
@@ -2,7 +2,7 @@
  * Created by wangxiaoqing on 2017/3/27.
  */
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./geo.prod');
+    module.exports = require('./geo.dev');
 } else {
     module.exports = require('./geo.dev');
 }
