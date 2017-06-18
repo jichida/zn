61bd492250af6ca194a3243eb5c9e8cba649e2f1
wangxiaoqing <xiaoqingwang@126.com>
2017-06-18 08:54:31 +0800

    push

--- test/src/app/App.js
+++ test/src/app/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as xview from '../utils/Common.js';
-import {alipayUrl,wxpayUrl,geographyLocationCallbackMethod} from '../utils/Common.js';
+import * as xview from '../utils/Common';
 
 let sourceData={"title":"夏恒网络XVIEW分享", "descrption":"夏恒网络XVIEW分享。", "picture":"https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2378550344,2476789148&fm=58", "url":"http://www.xiaheng.net/"};
 
@@ -11,52 +10,55 @@ class App extends Component {
   constructor(props) {
 		super(props);
     this.state={txName:''}
+
+    window.webBack=function(){
+  		try{
+        //console.log(instances);
+        alert("window.webBack");
+        //监听 安卓返回按钮 的触发事件
+
+        // if($("body").hasOverlay()){
+        //   $("body").distoryOverlay();
+        // }else{
+        //   if(navigator.pages.length>1){
+    		// 		navigator.popPage();
+    		// 	}else{
+    		// 		window.confirm("确认退出？",function(){
+    		// 			Common.exitApp();
+    		// 		});
+    		// 	}
+        // }
+  		}catch(e){
+        console.log(e);
+  		}
+  	}
+
 	}
 
   zfbPay=()=>{
     let param="partner=\"2088521376976114\"&seller_id=\"1742688357@qq.com\"&out_trade_no=\"PC_D1488616952710292\"&subject=\"PC_D1488616952710292\"&body=\"XVIEW alipay\"&total_fee=\"10.01\"&notify_url=\"http://www.yaoquan.com28.cn/servlet/NotifyAlipayServlet\"&service=\"mobile.securitypay.pay\"&payment_type=\"1\"&_input_charset=\"utf-8\"&it_b_pay=\"30m\"&sign=\"Ee%2Fo%2B%2BGhOpktpKDw1iJ51glG9wdd0UJ2gji0z5VNeBPZRuKj7OHVp0S%2F6XOgUK7xrPJVqUFWuO06%2Fr4AYzMumH%2F4jI%2B2G3OULhDONmJsBJYCz4%2FVZk9TTWIY7YVlH5Kl28ztJRS5KsfpPbOvXpCytTYwVcydGgVvzDhRXsGIVq8%3D\"&sign_type=\"RSA\"";
     alert(param);
-    alipayUrl(param,(data)=>{
+    xview.alipayUrl(param,(data)=>{
       alert(JSON.stringify(data));
     });
   }
   wxPay=()=>{
     let param={appid:"wx2be56c97af373c6b",noncestr:"flOaJHzueF6YaoYq",package:"Sign=WXPay",partnerid:"1420780402",prepayid:"wx2017030416453305ddbf9b350712243602",sign:"5EE3E0C56F9C2F0AEC95AB2FF78DD25F",timestamp:"1488617132"};
     alert(JSON.stringify(param));
-    wxpayUrl(param,(data)=>{
+    xview.wxpayUrl(param,(data)=>{
       alert(JSON.stringify(data));
     });
   }
-  getLocation(){
+  getLocation=()=>{
     try{
-      alert(`window.xview:${!!window.xview}`);
-      alert(`geographyLocationCallbackMethod:${!!window.xview.geographyLocationCallbackMethod}`);
-
-      window.geographyLocation2=function(result){
-        window.alert(JSON.stringify(result));
-      };
-      window.xview.geographyLocationCallbackMethod("geographyLocation2");
+      xview.geographyLocationCallbackMethod((data)=>{
+        alert(JSON.stringify(data));
+      })
     }catch(e){
-      window.alert(e.message);
+      alert(e.message);
     }
   }
 
-  getLocation2(){
-    alert(`??window.xview:${!!window.xview}`);
-    alert(`??geographyLocationCallbackMethod:${!!window.xview.geographyLocationCallbackMethod}`);
-
-  }
-
-  getLocation3(){
-    window.location.href = 'http://www.ibltz.weixingang.com';
-    // try{
-    //   xview.geographyLocationCallbackMethod((data)=>{
-    //     alert(JSON.stringify(data));
-    //   })
-    // }catch(e){
-    //   alert(e.message);
-    // }
-  }
 
   loginQQ=()=>{
     xview.loginToTencentQQ((data)=>{
@@ -142,6 +144,10 @@ class App extends Component {
     this.setState({txName:event.target.value})
   }
 
+  exitApp=()=>{
+    xview.exitApp();
+  }
+
   render() {
     return (
       <div className="App">
@@ -154,9 +160,7 @@ class App extends Component {
           <br/>
           <button onClick={ this.zfbPay }>支付宝支付</button>
           <button onClick={ this.wxPay }>微信支付</button>
-          <button onClick={ this.getLocation.bind(this) }>获取定位</button>
-          <button onClick={ this.getLocation2.bind(this) }>获取定位2</button>
-          <button onClick={ this.getLocation3.bind(this) }>获取定位2</button>
+          <button onClick={ this.getLocation }>获取定位</button>
 
           <br/>
           <button onClick={ this.loginQQ }>QQ登录</button>
@@ -173,6 +177,9 @@ class App extends Component {
           <button onClick={ this.prepareEasyLink }>连接mico</button>
           <button onClick={ this.searchForModules }>搜索mico</button>
           <button onClick={ this.getPhoneBook }>通讯录</button>
+
+          <br/>
+          <button onClick={ this.exitApp }>退出App</button>
 
           <br/><br/>
           <input type="text" value={this.state.txName} onChange={ this.setPushName } />
