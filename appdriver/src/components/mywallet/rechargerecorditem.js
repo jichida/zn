c007493cab0291e7ff2631997be5dc111d64f8fa
wangxiaoqing <xiaoqingwang@126.com>
2017-06-20 17:32:21 +0800

    提现修改

--- appdriver/src/components/mywallet/rechargerecorditem.js
+++ appdriver/src/components/mywallet/rechargerecorditem.js
@@ -13,11 +13,29 @@ import moment from 'moment';
 
 let Item =(props)=> {
   const {record} = props;
+  let srctypename;
+  if(record.srctype === 'order'){
+    srctypename = _.get(record,'fromorder.triptype');
+  }
+  else{
+    if(_.startsWith(record.srctype,'withdrawcash')){
+      srctypename = '提现申请';
+      let typemap = {
+        'withdrawcash':'提现申请',
+        'withdrawcash_ing':'提现中',
+        'withdrawcash_ed':'提现成功',
+        'withdrawcash_denied':'提现失败',
+      };
+      if(!!typemap[record.srctype]){
+        srctypename = typemap[record.srctype];
+      }
+    }
+  }
   return (
     <Cell key={record._id}>
       <CellBody>
           <span className="time">{moment(record.created_at).format("YYYY-MM-DD HH:mm:ss")}</span>
-          <span className="status">{_.get(record,'fromorder.triptype')}</span>
+          <span className="status">{srctypename}</span>
       </CellBody>
       <CellFooter>
           <span className="color_warning">{record.feebonus>0?"+":""}{record.feebonus}</span>
