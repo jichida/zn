3382d0cfa4e8b27884a895532bb6b6b6d926a50e
wangxiaoqing <xiaoqingwang@126.com>
2017-06-23 16:59:53 +0800

    BTN 按钮

--- admin/src/components/userdrivers/btn.js
+++ admin/src/components/userdrivers/btn.js
@@ -25,27 +25,22 @@ class ApproveButton extends Component {
 
     render() {
         const { record } = this.props;
-        let co;
-
-        if(record.approvalstatus === '待审核'){
-            co = (<span>
-              <FlatButton label="开始审核" primary={true}  onClick={this.handleStartApprove} />
-            </span>);
+        let co = (<span></span>);
+        if(!!record){
+          if(record.approvalstatus === '待审核'){
+              co = (<span>
+                <FlatButton label="开始审核" primary={true}  onClick={this.handleStartApprove} />
+              </span>);
+          }
+          else if(record.approvalstatus !== '未递交'){
+            co = (
+                <span>
+                    <FlatButton label="通过" primary={true} onClick={this.handleApprove} disabled={record.approvalstatus === '已审核'}/>
+                    <FlatButton label="拒绝" secondary={true}  onClick={this.handleReject} disabled={record.approvalstatus === '已拒绝'}  />
+                </span>
+            );
+          }
         }
-        else if(record.approvalstatus !== '未递交'){
-          co = (
-              <span>
-                  <FlatButton label="通过" primary={true} onClick={this.handleApprove} disabled={record.approvalstatus === '已审核'}/>
-                  <FlatButton label="拒绝" secondary={true}  onClick={this.handleReject} disabled={record.approvalstatus === '已拒绝'}  />
-              </span>
-          );
-        }
-        else{
-          co = (<span>
-          未递交
-          </span>);
-        }
-
         return co;
     }
 }
