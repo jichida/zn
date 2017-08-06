c08b49760948aa4e97a1ec304828ad30ee06e439
wangxiaoqing <xiaoqingwang@126.com>
2017-08-06 13:40:46 +0800

    路线列表

--- pinchedriver/src/components/index.js
+++ pinchedriver/src/components/index.js
@@ -13,6 +13,7 @@ import { getmypincheroute_request, getmypincheroute_result } from '../actions';
 import {callthen} from '../sagas/sagacallback';
 import '../newcss/index.css';
 
+
 const { Cells, Cell, CellBody, CellFooter } = WeUI;
 let usecacherouter = false;
 
@@ -28,22 +29,39 @@ export class Page extends Component {
     }
 
     updateContent = (routerinfo)=> {
+        console.log(`routerinfo==>${JSON.stringify(routerinfo)}`);
+        // {"_id":"5953837600935b0352e1a472","isenabled":true,"pinchetype":"人气团拼","startcity":"南京",
+        // "endcity":"天长","startdate":"2017-08-09T16:00:00.000Z",
+        // "seatnumber":50,"__v":0,"carpoolprice":{"鼓楼":{"总站":50,"分站":51},
+        // "客运站":{"总站":60,"分站":61}},"starttime":"11:55","groupnumber":35,
+        // "carpoolstationtime":{"鼓楼":"18:00","客运站":"18:20","总站":"21:00","分站":"21:10"},
+        // "pinchedriveruserid":"597fedc8fb045406435074f8",
+        // "userlist":[],"status":"未成团","endstations":["总站","分站"],
+        // "startstations":["鼓楼","客运站"],"created_at":"2017-06-05T09:22:01.684Z"}
+        let {startdate,starttime,startcity,endcity,groupnumber,userlist,seatnumber} = routerinfo;
+        try{
+          startdate = moment(startdate).format("YYYY-MM-DD");
+        }
+        catch(e){
+
+        }
+        let starttimestring = `${startdate} ${starttime}`;
         return  (
             <div
                 className="licontent"
                 onClick={()=>{this.pagePush(`routerdetail/${routerinfo._id}`)}}
                 >
-                
-                <div className="time">2017-02-09</div>
+
+                <div className="time">{starttimestring}</div>
                 <div className="city">
-                    南京——天长
+                    {startcity}——{endcity}
                     <p className="text-warning margin-top-0">
-                        <span>20人成团</span>
-                        <span>载客20人</span>
-                        <span>10人已参与</span>
+                        <span>{groupnumber}人成团</span>
+                        <span>载客{seatnumber}人</span>
+                        <span>{userlist.length}人已参与</span>
                     </p>
                 </div>
-                    
+
             </div>
         );
     }
@@ -53,7 +71,6 @@ export class Page extends Component {
             <div className="indexPage AppPage">
                 <NavBar back={true} title="我的订单" />
                 <div className="list">
-                    <div>
                         <InfinitePage
                             usecache={usecacherouter}
                             listtypeid='routerinfo'
@@ -66,38 +83,6 @@ export class Page extends Component {
                             query={{}}
                             sort={{}}
                         />
-                        <div className="listcontent"
-                            onClick={()=>{this.pagePush(`routerdetail/1`)}}
-                            >
-                            <div className="licontent">
-                                <div className="time">02-09<br/>12:30</div>
-                                <div className="city">
-                                    南京——天长
-                                    <p className="text-warning margin-top-0">
-                                        <span>20人成团</span>
-                                        <span>载客20人</span>
-                                        <span>10人已参与</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="listcontent">
-                            <div className="licontent">
-                                <div className="time">02-09<br/>12:30</div>
-                                <div className="city">
-                                    南京——天长
-                                    <p className="text-warning margin-top-0">
-                                        <span>20人成团</span>
-                                        <span>载客20人</span>
-                                        <span>10人已参与</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    
                 </div>
             </div>
         )
