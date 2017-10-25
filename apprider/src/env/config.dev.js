ca4cc9a321a152e07e594cffb04107bcd68d8ada
wangxiaoqing <xiaoqingwang@126.com>
2017-10-25 13:26:01 +0800

    for test

--- apprider/src/env/config.dev.js
+++ apprider/src/env/config.dev.js
@@ -1,6 +1,6 @@
 let islocalhost = false;
 let config = {
-    serverurl:islocalhost?'http://localhost:3004':'http://zn.com28.cn:3004',
+    serverurl:islocalhost?'http://localhost:3004':'http://zn.tczncx.com/',
     requesttimeout:5000,
     intervalrequestnearbydriver:4000
 };
