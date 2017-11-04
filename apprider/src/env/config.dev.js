6e1cd4fbf3bba8bdc55e15372bc7622e7339173f
wangxiaoqing <xiaoqingwang@126.com>
2017-11-04 22:34:38 +0800

    loading问题

--- apprider/src/env/config.dev.js
+++ apprider/src/env/config.dev.js
@@ -2,7 +2,7 @@ let islocalhost = false;
 let config = {
     serverurl:islocalhost?'http://localhost:3004':'http://api.tczncx.com/',
     requesttimeout:5000,
-    intervalrequestnearbydriver:4000
+    intervalrequestnearbydriver:5000
 };
 
 export default config;
--- apprider/src/env/config.prod.js
+++ apprider/src/env/config.prod.js
@@ -4,7 +4,7 @@
 let config = {
     serverurl:'http://api.tczncx.com/',//'http://localhost:3100',//'http://vpn.czjcd.com:3100',//
     requesttimeout:5000,
-    intervalrequestnearbydriver:3000
+    intervalrequestnearbydriver:5000
 };
 
 export default config;
