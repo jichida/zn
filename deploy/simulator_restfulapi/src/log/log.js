b0b05998888146716b9be5a67db42f457a7d871a
wangxiaoqing <xiaoqingwang@126.com>
2017-12-29 12:36:02 +0800

    新增log文件

---
+++ deploy/simulator_restfulapi/src/log/log.js
@@ -0,0 +1,52 @@
+var winston = require('winston');
+var moment = require('moment');
+var path = require('path');
+const config = {
+  logdir:'../../dist/log'
+};
+
+var logger;
+exports.initLog =  ()=>{
+  var filename = 'simulatorsrv';//"simulatorsrv_"+moment().format('YYYY-MM-DD-HHmmss');
+
+  var logfile = `${config.logdir}/${filename}.log`;
+  var logpath = path.resolve(__dirname,'../', logfile);
+  console.log(`logpath==>${logpath}`);
+
+  var logfileerr = `${config.logdir}/${filename}_err.log`;
+  var logpatherr = path.resolve(__dirname,'../', logfileerr);
+
+  var logfilewarn = `${config.logdir}/${filename}_warn.log`;
+  var logpathwarn = path.resolve(__dirname,'../', logfilewarn);
+
+  // winston.configure({
+  //   transports: [
+  //     new (winston.transports.File)({ filename: logpath ,level: 'info'}),
+  //     new (winston.transports.File)({  filename: logfileerr, level: 'error'  }),
+  //   ]
+  // });
+
+    logger = new (winston.Logger)({
+      transports: [
+        new (winston.transports.File)({
+          name: 'info-file',
+          filename: logpath ,
+          level: 'info'
+        }),
+        new (winston.transports.File)({
+          name: 'error-file',
+          filename: logpatherr,
+          level: 'error'
+        }),
+        new (winston.transports.File)({
+          name: 'warn-file',
+          filename: logpathwarn,
+          level: 'warn'
+        }),
+      ]
+  });
+};
+
+exports.getlog = ()=>{
+   return logger;
+}
