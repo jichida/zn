9542daf36ee87dfa154f3da13642b71b0779c098
wangxiaoqing <xiaoqingwang@126.com>
2018-02-16 19:26:35 +0800

    deploy

--- deploy/uploadsrv/src/config.js
+++ deploy/uploadsrv/src/config.js
@@ -1,15 +1,15 @@
 const config = {
   CompanyId:'zncx',
-  logdir:process.env.logdir||'../../dist/log',
+  logdir:process.env.logdir||'/app/zn/logs',
   platformserverurl:process.env.platformserverurl||'http://127.0.0.1:8080',//'http://127.0.0.1:8080',
   srvsftp:{
-      host: 'vpn.czjcd.com',
-      port: '22',
-      username: 'root',
-      password: 'DownUp2U'
+      host: process.env.srvsftp_host||'vpn.czjcd.com',
+      port: process.env.srvsftp_port||'22',
+      username: process.env.srvsftp_username||'root',
+      password: process.env.srvsftp_password||'DownUp2U'
   },
   srvredis:{
-    host:'192.168.0.1'
+    host:process.env.srvredis_host||'192.168.0.1'
   }
 };
 
