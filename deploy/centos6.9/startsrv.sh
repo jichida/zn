066133c9bed025a20ca89d56ae6cb55f965a0f32
wangxiaoqing <xiaoqingwang@126.com>
2018-03-05 17:47:32 +0800

    sh

--- deploy/centos6.9/startsrv.sh
+++ deploy/centos6.9/startsrv.sh
@@ -1,6 +1,6 @@
-docker-compose  -f ./mongodb.yml  up
-docker-compose  -f ./redis.yml up
-docker-compose  -f ./simulatorsrv.yml up
-docker-compose  -f ./uploadsrv.yml up
-docker-compose  -f ./znserver.yml up
-docker-compose  -f ./nginx.yml up
+docker-compose  -f ./mongodb.yml  up -d
+docker-compose  -f ./redis.yml up -d
+docker-compose  -f ./simulatorsrv.yml up -d
+docker-compose  -f ./uploadsrv.yml up -d
+docker-compose  -f ./znserver.yml up -d
+docker-compose  -f ./nginx.yml up -d
