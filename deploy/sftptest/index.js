60f0c6d6f042e17aecd2befac6bf08b2e96d3f54
wangxiaoqing <xiaoqingwang@126.com>
2018-03-07 13:44:00 +0800

    index

---
+++ deploy/sftptest/index.js
@@ -0,0 +1,11 @@
+const sftptosrv =  require('./src/sftp/index.js');
+
+const localdir = `/app/zn/deploy/dist/uploader`;
+const localfilename = `fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg`;
+
+console.log(`start....`);
+sftptosrv(localdir,localfilename,(err,result)=>{
+  console.log(`sftptosrv--->callback`);
+  console.log(err);
+  console.log(result);
+});
