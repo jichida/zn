48d67dd4f0cdfc609814d34b6c63f794739be238
wangxiaoqing <xiaoqingwang@126.com>
2018-03-10 16:16:03 +0800

    log err

--- deploy/uploadsrv/src/restfulapi/index.js
+++ deploy/uploadsrv/src/restfulapi/index.js
@@ -37,7 +37,7 @@ const uploadtoplatform = (IPCType,uri,data)=>{
       } else {
         debug(`${IPCType}-->Error`);
         winston.getlog().error(`${IPCType}-->Error`);
-        winston.getlog().error(response.body.toString());
+        winston.getlog().error(response.text());
         return Promise.reject()
       }
     })
