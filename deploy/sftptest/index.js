c15aaa4293bf439df59a5dd5eb8ada611302fa91
wangxiaoqing <xiaoqingwang@126.com>
2018-07-12 02:21:19 +0800

    to config

--- deploy/sftptest/index.js
+++ deploy/sftptest/index.js
@@ -1,11 +1,7 @@
 const sftptosrv =  require('./src/ftps/index.js');
+const config = require('./src/config');
 
-const localdir = `/app/zn/deploy/dist/uploader`;
-const localfilename = `fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg`;
-
-const remotefilename = `test.jpeg`;
-const collectionname = `baseinfodriver`;
-sftptosrv(localdir,localfilename,collectionname,remotefilename,(err,result)=>{
+sftptosrv(config.localdir,config.localfilename,config.remotedir,config.remotefilename,(err,result)=>{
   console.log(err);
   console.log(result);
 });
--- deploy/sftptest/src/config.js
+++ deploy/sftptest/src/config.js
@@ -2,6 +2,10 @@ const config = {
   CompanyId:'3411ZNCX293T',
   logdir:process.env.logdir||'/app/zn/logs',
   uploaddir:process.env.uploaddir,
+  localdir:process.env.localdir || `/app/zn/deploy/dist/uploader`,
+  localfilename:process.env.localfilename || `fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg`,
+  remotedir:process.env.remotedir || `/home/wycftp1079/baseinfodriver`,
+  remotefilename:process.env.remotefilename || `test.jpeg`,
   platformserverurl:process.env.platformserverurl||'http://172.16.5.101:8080',//'http://127.0.0.1:8080',
   srvsftp:{
       host: process.env.srvsftp_host||'vpn.i2u.top',
