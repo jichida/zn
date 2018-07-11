a4bb77f2e8242be8a4ae084e6ab68982e9d53e93
wangxiaoqing <xiaoqingwang@126.com>
2018-07-12 02:05:00 +0800

    for test

--- deploy/sftptest/index.js
+++ deploy/sftptest/index.js
@@ -4,8 +4,15 @@ const localdir = `/app/zn/deploy/dist/uploader`;
 const localfilename = `fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg`;
 
 const remotefilename = `test.jpeg`;
-const collectionname = `/home/wycftp1079`;
+const collectionname = `baseinfodriver`;
 sftptosrv(localdir,localfilename,collectionname,remotefilename,(err,result)=>{
   console.log(err);
   console.log(result);
 });
+
+/*
+lftp sftp://wycftp1079@vpn.i2u.top
+@N5755OR8c7!
+put -O /home/wycftp1079/baseinfocompany /app/zn/deploy/dist/uploader/fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg
+put /app/zn/deploy/dist/uploader/fc1f8e88-f3a5-4a04-817b-1a61b5f77ed2.jpeg
+*/
